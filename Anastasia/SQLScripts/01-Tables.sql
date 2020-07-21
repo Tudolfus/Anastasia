24813d657e7790810527e9ff81d4dc6c970fe6d4
tekoteko <RamaPapa29@yandex.ru>
2020-07-22 00:21:54 +0300

    db migration setup

---
+++ Anastasia/SQLScripts/01-Tables.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE dbo.Users(
+	UserId int IDENTITY(1,1) NOT NULL,
+	Login nvarchar(100) NOT NULL
+) 
+GO
--- Anastasia/Startup.cs
+++ Anastasia/Startup.cs
@@ -9,18 +9,40 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DbUp;
+using Microsoft.Extensions.Configuration;
 
 namespace Anastasia
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader = DeployChanges.To.SqlDatabase(connectionString, null)
+                .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
+                .WithTransaction()
+                .Build();
+
+            if (upgrader.IsUpgradeRequired())
+            {
+                upgrader.PerformUpgrade();
+            }
+
             services.AddControllers();
+
             services.SetupDI();
-            //DIHelper.SetupDI(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +61,11 @@ namespace Anastasia
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private void EnableMigrationDatabase()
+        {
+
         }
     }
 }
